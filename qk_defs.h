a54195a8a4f6c796e030ed79256f955c221974fe
Mario Ribeiro <mario.ribas@gmail.com>
2013-10-31 00:03:54 +0000

    mini update, eclipse project added

---
+++ qk_defs.h
@@ -0,0 +1,11 @@
+#ifndef QK_DEFS_H
+#define QK_DEFS_H
+
+#define QK_FLAGMASK_EVENTNOTIF  (1<<0)
+#define QK_FLAGMASK_STATUSNOTIF (1<<1)
+#define QK_FLAGMASK_AUTOSAMP    (1<<2)
+
+#define QK_LABEL_SIZE       20
+#define QK_BOARD_NAME_SIZE  20
+
+#endif // QK_DEFS_H
